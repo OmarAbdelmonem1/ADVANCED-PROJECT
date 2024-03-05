922d680c163d504ca9dd70b31b2461267329da26
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2024-03-05 20:47:02 +0200

    read products from db in home

--- adv/src/main/java/com/adv/adv/controller/IndexController.java
+++ adv/src/main/java/com/adv/adv/controller/IndexController.java
@@ -1,14 +1,31 @@
 package com.adv.adv.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.adv.adv.model.Product;
+import com.adv.adv.repository.ProductRepository;
 
 
 @Controller
 public class IndexController {
+    // @GetMapping("/home")
+    // public String home() {
+    //     return "index";
+    // }
+    @Autowired
+    private ProductRepository productRepository;
+    
     @GetMapping("/home")
-    public String home() {
-        return "index";
+    public ModelAndView getAll() {
+        ModelAndView mav =new ModelAndView("index.html");
+        List<Product>products=this.productRepository.findAll();
+        mav.addObject("products", products);
+        return mav;
     }
     
 }
